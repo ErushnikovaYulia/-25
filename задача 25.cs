19a12b8b56009c22ff2d4c272ed3aa44e92d398d
Yulia Erushnikova <erushnikova.talisman@gmail.com>
2022-10-15 17:06:56 +0500

    task 25

---
+++ задача 25.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл который принимает на вход два числа (А и В) 
+// и возводит число А в натуральное число В.
+
+Console.WriteLine("Введите число A");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B");
+int b = int.Parse(Console.ReadLine()!);
+
+int X = a;
+
+for (int i = 1; i < b; i++)
+{
+X = X * a;
+}
+Console.WriteLine("A в степени B равно: " + X);
